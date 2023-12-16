81f2099e7453ee7c872348bfd943fb76ab6680c6
Ahsaan <ahsaanjee045@gmail.com>
2023-12-16 13:21:33 +0530

    added delete product, update product functionalities

--- src/slices/productSlice.js
+++ src/slices/productSlice.js
@@ -59,13 +59,58 @@ export const getSingleProduct = createAsyncThunk(
     }
 );
 
+export const deleteProduct = createAsyncThunk(
+    "products/deleteProduct",
+    async ({ productId, token }) => {
+        try {
+            let res = await axios.delete(`/products/${productId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (res.data.success) {
+                return res.data.data;
+            }
+            return Promise.reject("Error while deleting product");
+        } catch (error) {
+            console.log(
+                "ERROR IN DELETE SINGLE PRODUCTS FUNCTION IN PRODUCT SLICE",
+                error
+            );
+            throw new Error(error.response.data.message || error.message);
+        }
+    }
+);
+
+export const updateProduct = createAsyncThunk(
+    "products/updateProduct",
+    async({ productId, token, data }) => {
+        try {
+            let res = await axios.put(`/products/${productId}`, data, {
+                headers : {
+                    Authorization : `Bearer ${token}`
+                }
+            })
+
+            return res.data.data
+        } catch (error) {
+            console.log(
+                "ERROR IN UPDATE SINGLE PRODUCTS FUNCTION IN PRODUCT SLICE",
+                error
+            );
+            throw new Error(error.response.data.message || error.message);
+        }
+    }
+);
+
 const productSlice = createSlice({
     name: "products",
     initialState: {
         products: [],
         loading: false,
         error: null,
-        product : null
+        product: null,
     },
     // reducers must be purely synchronous
     reducers: {},
@@ -108,7 +153,47 @@ const productSlice = createSlice({
         });
         builder.addCase(getSingleProduct.rejected, (state, action) => {
             state.loading = false;
+            state.error = action.error;
+        });
+        builder.addCase(deleteProduct.pending, (state, action) => {
+            state.loading = true;
             state.error = null;
+        });
+        builder.addCase(deleteProduct.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = null;
+            // state.product = action.payload;
+            let pIndex = state.products.findIndex(
+                (p) => p._id === action.payload._id
+            );
+            if (pIndex !== -1) {
+                state.products.splice(pIndex, 1);
+                toast.success("Product Delete Successfully!");
+            }
+        });
+        builder.addCase(deleteProduct.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error;
+        });
+        builder.addCase(updateProduct.pending, (state, action) => {
+            state.loading = true;
+            state.error = null;
+        });
+        builder.addCase(updateProduct.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = null;
+            
+            let pIndex = state.products.findIndex(
+                (p) => p._id === action.payload._id
+            );
+            if (pIndex !== -1) {
+                state.products.splice(pIndex, 1, action.payload);
+                toast.success("Product Updated Successfully!");
+            }
+        });
+        builder.addCase(updateProduct.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error;
         });
     },
 });
